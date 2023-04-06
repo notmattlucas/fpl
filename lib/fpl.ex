1d5ce3d43bc2c686edf75eb143c8b606f73afd43
Matt Lucas <matt.lucas@nagra.com>
2023-04-06 17:36:49 +0100

    Initial commit

---
+++ lib/fpl.ex
@@ -0,0 +1,18 @@
+defmodule Fpl do
+  @moduledoc """
+  Documentation for `Fpl`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Fpl.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ mix.exs
@@ -0,0 +1,28 @@
+defmodule Fpl.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :fpl,
+      version: "0.1.0",
+      elixir: "~> 1.14",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:jason, "~> 1.4.0"},
+      {:httpoison, "~> 2.1.0"}
+    ]
+  end
+end
