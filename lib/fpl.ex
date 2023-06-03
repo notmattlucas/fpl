c20c1e5b244d61f169bf55eb785dc1a41a0b6171
Matt Lucas <matt.lucas@nagra.com>
2023-06-03 19:52:42 +0100

    Initial setup for calls

--- lib/fpl.ex
+++ lib/fpl.ex
@@ -1,18 +1,47 @@
+require HTTPoison
+
 defmodule Fpl do
   @moduledoc """
-  Documentation for `Fpl`.
+  A library to read data from the Fantasy Premier League API
   """
 
-  @doc """
-  Hello world.
+  @host "https://fantasy.premierleague.com"
 
-  ## Examples
-
-      iex> Fpl.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def bootstrap() do
+    req("/api/bootstrap-static/")
   end
+
+  def fixtures() do
+    req("/api/fixtures/")
+  end
+
+  def game_weeks() do
+    bootstrap()["events"]
+  end
+
+  def phases() do
+    bootstrap()["phases"]
+  end
+
+  def teams() do
+    bootstrap()["teams"]
+  end
+
+  def players() do
+    bootstrap()["elements"]
+  end
+
+  def player_info(id) do
+    req("/api/element-summary/#{to_string(id)}/")
+  end
+
+  def game_week_data(gw_id) do
+    req("/api/event/#{to_string(gw_id)}/live/")
+  end
+
+  defp req(url) do
+    response = HTTPoison.get!(@host <> url)
+    Jason.decode!(response.body)
+  end
+
 end
